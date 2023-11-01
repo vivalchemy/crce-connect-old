2c4d07e5fd06442bc2b05aeb6031e282f6920a72
vivalchemy <vivianludrick.social+github@gmail.com>
2023-11-01 16:32:22 +0530

    last try

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
@@ -10,9 +10,9 @@ const config = {
 	// for more information about preprocessors
 	preprocess: [vitePreprocess(), mdsvex(mdsvexConfig)],
 
-	// kit: {
+	kit: {
 		adapter: adapter()
-	// }
+	}
 };
 
 export default config;
